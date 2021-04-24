5267bcf88c33fdc1ff531cb68c17674ca117a34a
faisal <ahmadrifa11620@gmail.com>
2021-04-24 16:29:17 +0700

    menambahkan api pada url

--- routes/assetRoute.js
+++ routes/assetRoute.js
@@ -10,12 +10,12 @@ const express = require('express');
 const AssetController = require('../controllers/AssetController');
 const route = express.Router();
 
-route.get('/', AssetController.index);
-route.get('/list', AssetController.fetchAll);
-route.get('/list/:id', AssetController.fetchById);
-route.post('/add-data', AssetController.addData);
-route.put('/update-data', AssetController.updateData);
-route.get('/group-data', AssetController.groupData);
-route.delete('/delete-data/:id', AssetController.deleteData);
+route.get('/api/', AssetController.index);
+route.get('/api/list', AssetController.fetchAll);
+route.get('/api/list/:id', AssetController.fetchById);
+route.post('/api/add-data', AssetController.addData);
+route.put('/api/update-data', AssetController.updateData);
+route.get('/api/group-data', AssetController.groupData);
+route.delete('/api/delete-data/:id', AssetController.deleteData);
 
 module.exports = route;
